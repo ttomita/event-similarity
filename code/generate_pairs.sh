4af79e963ba6edd28de88263b930bff060196cf0
Tyler Tomita <tmtomita87@gmail.com>
2018-06-15 10:44:59 -0400

    replace one of Rachel's demo vids with one of my own

--- code/generate_pairs.sh
+++ code/generate_pairs.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
+NUMFILES=$(($(ls -lh ../videos/ | wc -l)-1))
 k=1;
-for ((i=1;i<=9;i++)); 
+for ((i=1;i<=$NUMFILES;i++)); 
 do 
-    for ((j=$((i+1));j<=9;j++));
+    for ((j=$((i+1));j<=$NUMFILES;j++));
     do
 	sed "s/Pair #1/Pair #$k/" pair.html > pair$k.html
 	sed -i '' "s/vida/vid$i/" pair$k.html
